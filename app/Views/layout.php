e5bffce84a12c43564544cda8684c446cdf4e93c
Etudiant <Etudiant>
2017-04-14 17:05:02 +0200

    Mobile first carousel V2

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -9,9 +9,9 @@
 
         <title>Accueil</title>
        
-        <!--________________________ Liens <________________________--></________________________-->
+        <!--________________________ Liens <________________________-->
+
         <!-- Bootstrap CDN -->
-        
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
         <!-- Custom CSS-->
@@ -144,5 +144,10 @@
             </div>
         </footer>
 	</div>
+
+    <!--________________________ JQuery pour Bootstrap ________________________-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!--________________________ Javascript pour Bootstrap ________________________-->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
