94df47beb93ee2c2db3bc943fe55f1677b66aaa6
Etudiant <Etudiant>
2017-04-14 18:23:37 +0200

    mise à jour fiche détaillée (Lanval)

--- app/Views/home/ficheDetaillee.php
+++ app/Views/home/ficheDetaillee.php
@@ -2,29 +2,57 @@
 
 <?php $this->start('main_content') ?>
 
-        <p id="idfilm"></p>
-        <p id="titre"></p>
-        <p id="synopsis"></p>
-        <p id="punchline"></p>
-        <div>Genres : <p id="genres" style="display: inline;"></p></div></br>
-        <p id="langue"></p>
-        <p id="dateDeSortie"></p>
-        <p id="PhotoRealisateur"></p>
-        <p id="realisateur"></p>
-        <p id="acteurs"></p>
-        <p id="affiche"></p></br>
-        <p>Acteurs : </p></br>
-        <p id="photoActeurs"></p>
-        <P id="bandeAnnonce"></p>
+<div class="container">
+            <div class="row">
 
-      <?php if(isset($nomFilm)):  ?>  
+                <!-- Titre du film-->
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="titre">
+                </div>
+
+                <!-- Affiche du film-->
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4" id="affiche">
+                </div>
+                
+                <!-- Lien Youtube du film-->
+                <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8" id="bandeAnnonce">
+                </div>
+
+                <!-- Synopsis -->
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="synopsis">
+                </div>
+
+                <!-- Photo du réalisateur -->
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="PhotoRealisateur">
+                </div>
+
+                <!-- Nom du réalisateur -->
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="realisateur">
+                </div>
+
+                 <!-- Punchline -->
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="punchline">
+                </div>
+
+                <!-- Genre -->
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="genres">
+                </div>
+
+                <!-- Langue -->
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="langue">
+                </div>
+
+                <!-- Date de sortie -->
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="dateDeSortie">
+                </div>
+
+            </div>
+        </div>
+
+<?php if(isset($nomFilm)):  ?>  
 <script>
     var Mike = "<?= $nomFilm ?>"
 </script>
 <script src="<?= $this->assetUrl("js/scriptMovieDB.js"); ?>"></script>
     <?php endif; ?>
-
-
-
 
 <?php $this->stop('main_content') ?>
